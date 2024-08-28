78f725622ffe7ea372c69a4e65c5b88b905a8e34
Juan Pablo Montoya B <mb.juanpablo@gmail.com>
2024-08-27 21:46:47 -0500

    Added additional model app layer validations for lenght

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
 
   # Validations
-  validates :account_key,  uniqueness: true, if: Proc.new { |user| user.account_key.present? }
-  validates :email,        presence: true, uniqueness: true
-  validates :key,          presence: true, uniqueness: true
-  validates :password,     presence: true
-  validates :phone_number, presence: true, uniqueness: true
+  validates :account_key,   length: { maximum: 100 }, uniqueness: true, if: Proc.new { |user| user.account_key.present? }
+  validates :email,         length: { maximum: 200 }, presence: true, uniqueness: true
+  validates :full_name,     length: { maximum: 200 }
+  validates :key,           length: { maximum: 100 }, presence: true, uniqueness: true
+  validates :password,      length: { maximum: 100 }, presence: true
+  validates :phone_number,  length: { maximum: 20 },  presence: true, uniqueness: true
 
   # Scopes
   default_scope -> { order(created_at: :desc) }
@@ -25,7 +26,7 @@ class User < ApplicationRecord
   private
 
   def generate_key
-    self.key = SecureRandom.hex(64)
+    self.key = SecureRandom.hex(32)
   end
 
   def request_account_key
